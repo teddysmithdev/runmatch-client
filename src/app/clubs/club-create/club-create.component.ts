0de759bf42c724160cc1a817ce9d946904a72942
Teddy Smith <teddysmithdev@gmail.com>
2021-02-03 11:29:24 -0500

    Club create added

---
+++ src/app/clubs/club-create/club-create.component.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import Stepper from 'bs-stepper';
+import { ToastrService } from 'ngx-toastr';
+import { User } from 'src/app/_models/user';
+import { ClubService } from 'src/app/_services/club.service';
+import { environment } from 'src/environments/environment';
+
+
+@Component({
+  selector: 'app-club-create',
+  templateUrl: './club-create.component.html',
+  styleUrls: ['./club-create.component.css']
+})
+export class ClubCreateComponent implements OnInit {
+  private stepper: Stepper;
+  clubCreateForm: FormGroup;
+  user: User;
+  baseUrl = environment.apiUrl;
+
+  constructor(
+    private clubService: ClubService, 
+    private toastr: ToastrService, 
+    private router: Router,
+    private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.stepper = new Stepper(document.querySelector("#stepper1"), {
+      linear: false,
+      animation: true,
+    });
+    this.initializeForm();
+  }
+
+  initializeForm() {
+    this.clubCreateForm = this.fb.group({
+      city: ['', Validators.required],
+      state: ['', Validators.required],
+      name: ['', Validators.required],
+      intro: ['', Validators.required],
+    })
+  }
+
+  next() {
+    this.stepper.next();
+  }
+
+  clubUpdate() {
+    this.clubService.createClub(this.clubCreateForm.value).subscribe(
+      (next) => {
+        console.log(this.user);
+        this.toastr.success('Club Created', 'Congratulations');
+        this.router.navigate(["/members"]);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+}
